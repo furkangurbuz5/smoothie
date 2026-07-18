c917cb482d8bfe2e2c5241762ccb1a669c36dbf6
furkan <furkangurbuz5@gmail.com>
2026-07-18 13:50:46 +0200

    Add simple form and css

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { SmoothieForm } from '../form/smoothie-form/smoothie-form';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [SmoothieForm],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('smoothie');
