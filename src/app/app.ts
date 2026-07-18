2a9afb0a12b81f3347a07cb18a5cdcddbc15afd9
furkan <furkangurbuz5@gmail.com>
2026-07-18 14:32:11 +0200

    signal and input practice

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
-import { Component, signal } from '@angular/core';
+import { Component, Signal, signal, WritableSignal } from '@angular/core';
 import { SmoothieForm } from '../form/smoothie-form/smoothie-form';
+import { MatSlideToggle, MatSlideToggleChange } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-root',
-  imports: [SmoothieForm],
+  imports: [SmoothieForm, MatSlideToggle],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('smoothie');
+
+  slideToggled: WritableSignal<boolean> = signal(false);
+
+  onSlideToggle(change: MatSlideToggleChange): void {
+    this.slideToggled.set(change.checked);
+  };
 }
